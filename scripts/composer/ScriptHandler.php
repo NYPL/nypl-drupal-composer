4d2733f0d895d67dd7ec09e84e03c13223be4cb4
Gregory Kallenberg <gregorykallenberg@nypl.org>
2019-08-19 11:39:35 -0400

    merge script routines for required files, etc. with temp nypl_emulsify file merge routine

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -32,18 +32,70 @@ class ScriptHandler
 
     // Required for unit testing
     foreach ($dirs as $dir) {
-      if (!$fs->exists($root . '/' . $dir)) {
-        $fs->mkdir($root . '/' . $dir);
-        $fs->touch($root . '/' . $dir . '/.gitkeep');
+      if (!$fs->exists($drupalRoot . '/' . $dir)) {
+        $fs->mkdir($drupalRoot . '/' . $dir);
+      }
+      $fs->touch($drupalRoot . '/' . $dir . '/.gitkeep');
+    }
+
+    // Prepare the settings file for installation
+    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') && $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
+      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
+      // Insert our homegrown settings file if it exists.
+      if ($fs->exists('/tmp/settings.php')) {
+        $fs->copy('/tmp/settings.php', $drupalRoot . '/sites/default/settings.php');
+        $event->getIO()->write("Copied custom /tmp/settings.php to sites/default");
+      }
+      else {
+        $event->getIO()->write("Failed to copy custom /tmp/settings.php to sites/default");
+      }
+      // Insert local settings for database connection and local development settings.
+      if ($fs->exists('/tmp/settings.local.php')) {
+        $fs->copy('/tmp/settings.local.php', $drupalRoot . '/sites/default/settings.local.php');
+        $fs->chmod($drupalRoot . '/sites/default/settings.local.php', 0666);
+        $event->getIO()->write("Copied /tmp/settings.local.php to sites/default");
+      }
+      else {
+        $event->getIO()->write("Failed to copy /tmp/settings.local.php to sites/default");
+      }
+      // Add the basic services.yml based on the default.services.yml file.
+      if ($fs->exists('/tmp/services.yml')) {
+        $fs->copy('/tmp/services.yml', $drupalRoot . '/sites/default/services.yml');
+        $fs->chmod($drupalRoot . '/sites/default/services.yml', 0666);
+        $event->getIO()->write("Copied /tmp/services.yml to sites/default");
+      }
+      else {
+        $event->getIO()->write("Failed to copy /tmp/services.yml to sites/default");
+      }
+      require_once $drupalRoot . '/core/includes/bootstrap.inc';
+      require_once $drupalRoot . '/core/includes/install.inc';
+      $settings['config_directories'] = [
+        CONFIG_SYNC_DIRECTORY => (object) [
+          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
+          'required' => TRUE,
+        ],
+      ];
+      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
+      if ($fs->exists($drupalRoot . '/sites/default/settings.php')) {
+        $event->getIO()->write("Created a sites/default/settings.php file with chmod 0666");
+      }
+      else {
+        $event->getIO()->write("Failed to create a sites/default/settings.php file with chmod 0666");
       }
     }
 
     // Create the files directory with chmod 0777
-    if (!$fs->exists($root . '/sites/default/files')) {
+    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
       $oldmask = umask(0);
-      $fs->mkdir($root . '/sites/default/files', 0777);
+      $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
       umask($oldmask);
-      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+      if ($fs->exists($drupalRoot . '/sites/default/files')) {
+        $event->getIO()->write("Created a sites/default/files directory with chmod 0777");
+      }
+      else {
+        $event->getIO()->write("Failed to create a sites/default/files directory with chmod 0777");
+      }
     }
   }
 
@@ -81,6 +133,42 @@ class ScriptHandler
     $gitignoreContents = file_get_contents($gitignoreFile);
     $gitignoreContents = preg_replace('/.*::: cut :::*/s', '', $gitignoreContents);
     file_put_contents($gitignoreFile, $gitignoreContents);
+  }
+  /**
+   * Checks if the installed version of Composer is compatible.
+   *
+   * Composer 1.0.0 and higher consider a `composer install` without having a
+   * lock file present as equal to `composer update`. We do not ship with a lock
+   * file to avoid merge conflicts downstream, meaning that if a project is
+   * installed with an older version of Composer the scaffolding of Drupal will
+   * not be triggered. We check this here instead of in drupal-scaffold to be
+   * able to give immediate feedback to the end user, rather than failing the
+   * installation after going through the lengthy process of compiling and
+   * downloading the Composer dependencies.
+   *
+   * @see https://github.com/composer/composer/pull/5035
+   */
+  public static function checkComposerVersion(Event $event) {
+    $composer = $event->getComposer();
+    $io = $event->getIO();
+
+    $version = $composer::VERSION;
+
+    // The dev-channel of composer uses the git revision as version number,
+    // try to the branch alias instead.
+    if (preg_match('/^[0-9a-f]{40}$/i', $version)) {
+      $version = $composer::BRANCH_ALIAS_VERSION;
+    }
+
+    // If Composer is installed through git we have no easy way to determine if
+    // it is new enough, just display a warning.
+    if ($version === '@package_version@' || $version === '@package_branch_alias_version@') {
+      $io->writeError('<warning>You are running a development version of Composer. If you experience problems, please update Composer to the latest stable version.</warning>');
+    }
+    elseif (Comparator::lessThan($version, '1.0.0')) {
+      $io->writeError('<error>Drupal-project requires Composer version 1.0.0 or higher. Please update your Composer before continuing</error>.');
+      exit(1);
+    }
   }
 
   /**
