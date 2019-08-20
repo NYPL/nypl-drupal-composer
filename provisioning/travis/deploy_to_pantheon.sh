1a49850bd4bca78c560323a5466f349075e89533
Gregory Kallenberg <gregorykallenberg@nypl.org>
2019-08-20 13:54:21 -0400

    add setup-environment script for pantheon deploy; remove open ssl directive

--- provisioning/travis/deploy_to_pantheon.sh
+++ provisioning/travis/deploy_to_pantheon.sh
@@ -5,9 +5,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "pantheon" ]; then
     # Setup password-less passphrase for Pantheon
-    openssl aes-256-cbc -K $encrypted_8a30a4e5a3eb_key -iv $encrypted_8a30a4e5a3eb_iv -in $TRAVIS_BUILD_DIR/pantheon-hash.txt.enc -out $TRAVIS_BUILD_DIR/pantheon-hash.txt -d
-    cp travis-ci-key ~/.ssh/id_rsa
-    chmod 0600 ~/.ssh/id_rsa
+    # openssl aes-256-cbc -K $encrypted_8a30a4e5a3eb_key -iv $encrypted_8a30a4e5a3eb_iv -in $TRAVIS_BUILD_DIR/pantheon-hash.txt.enc -out $TRAVIS_BUILD_DIR/pantheon-hash.txt -d
+    # cp travis-ci-key ~/.ssh/id_rsa
+    # chmod 0600 ~/.ssh/id_rsa
     echo "Deploying $TRAVIS_BRANCH"
     bash $TRAVIS_BUILD_DIR/bin/push-to-pantheon
   else
