59691e016f887603b2a84d6282bdd221ca00328e
Gregory Kallenberg <gregorykallenberg@nypl.org>
2019-08-23 14:29:03 -0400

    adjust order of travis cleanup commands; remove --autocreate directive from pantheon script invocation

--- provisioning/travis/deploy_to_pantheon.sh
+++ provisioning/travis/deploy_to_pantheon.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "pantheon" ]; then
     echo "Deploying $TRAVIS_BRANCH"
-    bash $TRAVIS_BUILD_DIR/bin/push-to-pantheon --autocreate
+    bash $TRAVIS_BUILD_DIR/bin/push-to-pantheon
   else
     echo "Skipping Pantheon deploy because it's not a deployable branch"
   fi
