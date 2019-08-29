3656e7c6af5f891e14217ec640a5419d211fe925
Gregory Kallenberg <gregorykallenberg@nypl.org>
2019-08-29 13:51:10 -0400

    add failsafe for scripts; adjust wording of output and add failure message

--- provisioning/travis/deploy_to_pantheon.sh
+++ provisioning/travis/deploy_to_pantheon.sh
@@ -1,8 +1,11 @@
 
 #! /bin/bash
+
+set -eo pipefail
+
 # Deploy only if we're testing the master branch
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "pantheon" ]; then
-  echo "Deploying $TRAVIS_BRANCH"
+  echo "Deploying branch: $TRAVIS_BRANCH"
   bash $TRAVIS_BUILD_DIR/bin/push-to-pantheon
 else
   echo "Skipping Pantheon deploy because it's not a deployable branch"
